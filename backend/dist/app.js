0baa291416967cb882bd9a69944c49c3f4d5eeae
KevshuppD <kcastrod@uft.edu>
2025-11-19 20:46:16 -0300

    separacios backend frontend

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const path_1 = __importDefault(require("path"));
 const database_1 = __importDefault(require("./config/database"));
 const cursos_1 = __importDefault(require("./routes/cursos"));
 const instructores_1 = __importDefault(require("./routes/instructores"));
@@ -18,16 +17,11 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
 // Routes
 app.use('/api/cursos', cursos_1.default);
 app.use('/api/instructores', instructores_1.default);
 app.use('/api/estudiantes', estudiantes_1.default);
 app.use('/api/inscripciones', inscripciones_1.default);
-// Basic route
-app.get('/', (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, '../public/index.html'));
-});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import connectDB from './config/database';
 import cursoRoutes from './routes/cursos';
 import instructorRoutes from './routes/instructores';
@@ -16,18 +15,12 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 app.use('/api/cursos', cursoRoutes);
 app.use('/api/instructores', instructorRoutes);
 app.use('/api/estudiantes', estudianteRoutes);
 app.use('/api/inscripciones', inscripcionRoutes);
-
-// Basic route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
